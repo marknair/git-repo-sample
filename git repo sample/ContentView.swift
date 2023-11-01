80c0b4803612010983c381823fc4d45518b87399
Mark Nair <marknair@me.com>
2023-11-01 11:25:22 -0500

    Second commit

--- git repo sample/ContentView.swift
+++ git repo sample/ContentView.swift
@@ -10,16 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "1.square.fill")
+            Image(systemName: "2.square.fill")
                 .resizable()
                 .scaledToFit()
-                .foregroundColor(.blue)
             
-            Text("Version 1.0")
+            Text("Version 2.0")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
+                
         }
         .padding()
+        .foregroundColor(.mint)
     }
 }
 
