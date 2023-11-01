ef3d0d65aa31dbe2bd94a2f40448c31d45ce6e6c
Mark Nair <marknair@me.com>
2023-11-01 10:38:23 -0500

    Initial Commit

---
+++ git repo sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  git repo sample
+//
+//  Created by Mark Nair on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ git repo sample/git_repo_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_repo_sampleApp.swift
+//  git repo sample
+//
+//  Created by Mark Nair on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct git_repo_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
